756878158737521839bd0a415b341b1e6e4364c9
Russell Purinton <russell.purinton@gmail.com>
2020-03-01 02:15:14 -0500

    api

--- html/index.php
+++ html/index.php
@@ -1,14 +1,7 @@
 <?php
 
-//bx/api/index.php
+//bx/html/index.php
 $bxdir = __DIR__ . "/..";
 require_once("$bxdir/modules/bx.php");
-
-if (!isset($_POST)) bxapi_failure("no post");
-if (!isset($_POST["packets"])) bxapi_failure("no packets in post");
-if (!$bx["api"]["packets"] = json_decode($_POST["packets"], true))
-        bxapi_failure("packets parse error");
-if (!sizeof($bx["api"]["packets"])) bxapi_failure("packets empty");
-
-bxapi_process_packets();
+bxapi_post();
 
